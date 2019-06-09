04644b01646a76eb214211570471f3a2a23cbe11
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-09 14:36:33 -0500

    Gif images and rating now appear in html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var foodList = ["kale", "sushi", "coffee", "rose", "avocado", "tofu", "kombucha"
 // Function for rendering the HTML to display the appropriate content
 function displayFoodGifs() {
     var food = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=54LeYFO9lvGP5gxdBJg5XsqmFnCFwb38"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=54LeYFO9lvGP5gxdBJg5XsqmFnCFwb38&limit=10"
 
     // Creates AJAX call for the specific food button being clicked
     $.ajax({
@@ -13,17 +13,27 @@ function displayFoodGifs() {
         method: "GET"
       }).then(function(response) {
           console.log(response);
-          var gifDiv = $("#gif-view");
-          gifDiv.append(response.data[0]);
           console.log(response.data[0].rating)
         // gifDiv.text(JSON.stringify(response));
         // var gifDiv = $("#gif-view");
         // gifDiv.append(response.rating);
 
-        //   $("<p>").text("Rated: " + response.rating),
-        //   $("<img>").attr("src" + response.img)
+        for (var i = 0; i < response.data.length; i++) {
+            var giphyPhoto = response.data[i].images.fixed_height_still.url;
+            var newImg = $("<img>");
+            newImg.attr("src", giphyPhoto);
+            $("#gif-view").prepend(newImg);
+            var rating = response.data[i].rating;
+            $("#gif-view").prepend("Rated: " + rating)
+        }
         
-    
+        // var gifDiv = $("<div>");
+        // gifDiv.append(
+        //   $("<p>").text("Rated: " + response.data[0].rating),
+        //   $("<img>").attr("src" + response.data[0].images.fixed_height.url)
+        // )
+        
+        // $("#gif-view").prepend(gifDiv);
     })
 }
 displayFoodGifs();
@@ -34,10 +44,8 @@ function renderButtons() {
     // Deleting the movie buttons prior to adding new movie buttons
     $("#buttons-view").empty();
 
-    // Looping through the array of food
+    // Looping through the array of food and generating buttons for each
     for (var i = 0; i < foodList.length; i++) {
-
-        // Generating buttons for each movie in the array.
         var a = $("<button>");
         // Adding a class
         a.addClass("food");
